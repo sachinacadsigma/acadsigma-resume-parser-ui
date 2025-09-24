5e2b6bccb3164fe0f515e61030f918a3e8434796
Dinesh Rout <49337110+DineshRout779@users.noreply.github.com>
2025-09-24 20:24:19 +0530

    added env logs

--- src/utils/razorpay.ts
+++ src/utils/razorpay.ts
@@ -3,6 +3,8 @@ import Razorpay from 'razorpay';
 export const razorpay_key_id = import.meta.env.VITE_RAZORPAY_KEY_ID;
 export const razorpay_key_secret = import.meta.env.VITE_RAZORPAY_KEY_SECRET;
 
+console.log('env=> ',import.meta.env);
+
 const razorpay = new Razorpay({
   key_id: razorpay_key_id,
   key_secret: razorpay_key_secret,
