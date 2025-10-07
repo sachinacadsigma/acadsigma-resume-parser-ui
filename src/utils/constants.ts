7cdeaa4ccb8471878ee685ddba9df0342e20321d
Dinesh Rout <rddinesh779@gmail.com>
2025-10-08 00:03:30 +0530

    fix: api url

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,2 +1,4 @@
-export const API_URL = import.meta.env.VITE_APP_API_URL;
+export const API_URL =
+  import.meta.env.VITE_APP_API_URL ||
+  'https://acadsigma-resume-parser.azurewebsites.net';
 export const GOOGLE_AUTH_URL = import.meta.env.VITE_GOOGLE_AUTH_API;
